b4a46567587f330d4106c46241f44a9d96312f57
Dinaris23 <zzzdinariszzz@gmail.com>
2018-04-12 15:14:10 +0300

    add mobile menu

--- app/js/layout/main.js
+++ app/js/layout/main.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function(){
         $('.burger_l__top').toggleClass('active');
         $('.burger_l__middle').toggleClass('active');
         $('.burger_l__bottom').toggleClass('active');
-        $('.header_nav').slideToggle(300, function(){
+        $('.m_nav').slideToggle(300, function(){
             if($(this).css('display') === 'none') {
                 $(this).removeAttr('style')
                 }
