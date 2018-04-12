6a53aa95a11d8d43d2eb7c5ac055b081d49e0d9b
Dinaris23 <zzzdinariszzz@gmail.com>
2018-04-12 17:38:57 +0300

    fixed header button

--- app/js/layout/main.js
+++ app/js/layout/main.js
@@ -1,7 +1,7 @@
 TweenMax.fromTo('.header_title', 0.7, {y:-400, opacity: 0}, {y: 0, opacity: 1, ease:Back.easeOut.config(2)});
 TweenMax.fromTo('.header_nav', 1, {delay: 2, x: 1000}, {x: 0});
-TweenMax.staggerFromTo('.header_menu_item', 1, { scale:0, delay:6},{ scale:1, ease:Back.easeIn}, 0.05);
-TweenMax.staggerFromTo('.header_submenu_item', 1, { scale:0, delay:6},{ scale:1, ease:Back.easeIn}, 0.05);
+TweenMax.staggerFromTo('.header_menu_item', 1, { scale:0},{ scale:1, delay:0.7, ease:Back.easeInOut}, 0.05);
+TweenMax.staggerFromTo('.header_submenu_item', 1, { scale:0},{ scale:1, delay:0.7, ease:Back.easeInOut}, 0.05);
 TweenMax.fromTo('.burger', 1, {y:-50}, {y: 0, ease:Back.easeInOut.config(2)});
 TweenMax.fromTo('.header_btn', 1, {scale:0}, {delay: 1.5, scale:1});
 
